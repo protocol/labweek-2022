8402bd062897e93f4971e8773c79ade18d52992f
Tom Benbow <tom@hellomoon.net>
2022-10-10 11:24:03 -0500

    Fix attribute warnings

--- website/components/video.js
+++ website/components/video.js
@@ -7,7 +7,7 @@ export default function Video({src}) {
     <div>
       <div className="mb-20 lg:mt-12 lg:mb-28 w-full max-w-6xl mx-auto">
         <div className="relative w-full h-0 lg:overflow-hidden lg:rounded-xl" style={{paddingTop: "56.25%"}}>
-          <iframe className="absolute inset-0" width="100%" height="100%" src={src} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <iframe className="absolute inset-0" width="100%" height="100%" src={src} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
       </div>
     </div>
